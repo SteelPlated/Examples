6a4908a9d6e332846caf74b7777317777b7181dc
SteelPlated <mix.singatulin@yandex.ru>
2022-05-01 13:04:37 +0500

    example 005 if else

---
+++ Example005_If else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter name user: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "steelplated")
+{
+    Console.WriteLine("quality assurance");
+}
+else
+{
+    Console.Write("Q, ");
+    Console.WriteLine(username);
+}
